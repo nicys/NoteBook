b390f9c69cf02eaed10b6d2dc0c1f9f3e8c1fed0
Nickolay Martynchuk <nicys2476@yandex.ru>
2021-02-08 14:15:20 +0700

    Added methods in NoteBook.kt

--- src/main/kotlin/NoteBook.kt
+++ src/main/kotlin/NoteBook.kt
@@ -1,6 +1,6 @@
 object NoteBook {
-    var notes = emptyArray<Note>()
-    var comments = emptyArray<Comment>()
+    var notes = listOf<Note>()
+    var comments = listOf<Comment>()
 
     fun addNote(note: Note): Note {
         notes += note.copy(id = notes.size + 1, deleted = false)
@@ -11,9 +11,7 @@ object NoteBook {
         for ((index, note) in notes.withIndex()) {
             if (!note.deleted) {
                 if (id == note.id) {
-
-                    comments += comment.copy(commentId = comments.size, message = comment.message,
-                        count = comment.count + 1, deleted = false)
+                    comments += comment.copy(commentId = comments.size + 1, message = comment.message, deleted = false)
                     return comments.last()
                 }
             }
@@ -22,11 +20,12 @@ object NoteBook {
     }
 
     fun deleteNote(id: Int): Boolean {
+//        notes.forEachIndexed { index, note ->
         for ((index, note) in notes.withIndex()) {
             if (!note.deleted) {
                 if (id == note.id) {
                     val newNote = note.copy(id = note.id, deleted = true)
-                    notes[index] = newNote
+                    val note = newNote
                     return true
                 }
             }
@@ -34,9 +33,95 @@ object NoteBook {
         throw NoteNotFoundException("Заметки с таким ID не существует!")
     }
 
+    fun deleteComment(idComment: Int): Boolean {
+        for ((index, comment) in comments.withIndex()) {
+            if (!comment.deleted) {
+                if (idComment == comment.commentId) {
+                    val newComment = comment.copy(commentId = comment.commentId, deleted = true)
+                    val comment = newComment
+                    return true
+                }
+            }
+        }
+        throw CommentNotFoundException("Комментария с таким ID не существует!")
+    }
+
+    fun editNote(id: Int): Boolean {
+        for ((index, note) in notes.withIndex()) {
+            if (!note.deleted) {
+                if (id == note.id) {
+                    val newNote = note.copy(id = note.id)
+                    val note = newNote
+                    return true
+                }
+            }
+        }
+        throw NoteNotFoundException("Заметки с таким ID не существует!")
+    }
+
+    fun editComment(idComment: Int): Boolean {
+        for ((index, comment) in comments.withIndex()) {
+            if (!comment.deleted) {
+                if (idComment == comment.commentId) {
+                    val newComment = comment.copy(commentId = comment.commentId)
+                    val comment = newComment
+                    return true
+                }
+            }
+        }
+        throw CommentNotFoundException("Комментария с таким ID не существует!")
+    }
+
+    fun get(idUser: Int) {
+        for (note in notes) {
+            if (!note.deleted) {
+                if (idUser == note.userId) {
+                    println("Note of User with ID: ${note.userId} - ${note.title}")
+                }
+            }
+        }
+        throw UserNotFoundException("Пользователя с таким ID не существует!")
+    }
+
+    fun getById(id: Int) {
+        for (note in notes) {
+            if (id == note.id) {
+                println("""|        title: ${note.title}
+                    |   text: ${note.text}
+                """.trimMargin())
+            }
+        }
+        throw NoteNotFoundException("Заметки с таким ID не существует!")
+    }
+
+    fun getComments(id: Int) {
+        for (note in notes) {
+            if (id == note.id) {
+                if (!note.deleted) {
+                    println(note.comment)
+                }
+            }
+        }
+        throw NoteNotFoundException("Заметки с таким ID не существует!")
+    }
+
+    fun restoreComment(idComment: Int): Boolean {
+        for (comment in comments) {
+            if (idComment == comment.commentId) {
+                if (comment.deleted) {
+                    val newComment = comment.copy(commentId = comment.commentId, deleted = false)
+                    val comment = newComment
+                    return true
+                }
+            }
+        }
+        throw CommentNotFoundException("Удаленного комментария с таким ID не существует!")
+    }
 
 
 
 }
 class NoteNotFoundException(message: String) : RuntimeException()
+class CommentNotFoundException(message: String) : RuntimeException()
+class UserNotFoundException(message: String) : RuntimeException()
 
