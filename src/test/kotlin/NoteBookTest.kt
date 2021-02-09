61bcef364cea4c3fa806c11ed1f40f700c741f25
Nickolay Martynchuk <nicys2476@yandex.ru>
2021-02-10 00:09:20 +0700

    NoteBookTest.kt added.

---
+++ src/test/kotlin/NoteBookTest.kt
@@ -0,0 +1,46 @@
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class NoteBookTest {
+
+    @Test
+    fun addNote() {
+    }
+
+    @Test
+    fun addComment() {
+    }
+
+    @Test
+    fun deleteNote() {
+    }
+
+    @Test
+    fun deleteComment() {
+    }
+
+    @Test
+    fun editNote() {
+    }
+
+    @Test
+    fun editComment() {
+    }
+
+    @Test
+    fun getNotes() {
+    }
+
+    @Test
+    fun getById() {
+    }
+
+    @Test
+    fun getComments() {
+    }
+
+    @Test
+    fun restoreComment() {
+    }
+}
