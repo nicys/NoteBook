39b0ed5392a0c1230d82bcb5647e357edc9fa727
Nickolay Martynchuk <nicys2476@yandex.ru>
2021-02-09 20:09:49 +0700

    Create interface NoteInterface.kt

--- src/main/kotlin/NoteInterface.kt
+++ src/main/kotlin/NoteInterface.kt
@@ -1,14 +1,24 @@
-interface NoteInterface {
+interface NoteInterface<N, C> {
 
-    fun addNote()
-    fun createComment()
-    fun deleteNote()
-    fun deleteComment()
-    fun editNote()
-    fun editComment()
-    fun get()
-    fun getById()
-    fun getComments()
-    fun restoreComment()
+    fun addNote(n: N): N
+
+    fun addComment(i: Int, c: C): C
+
+    fun deleteNote(i: Int): Boolean
+
+    fun deleteComment(i: Int): Boolean
+
+    fun editNote(i: Int, n: N): Boolean
+
+    fun editComment(i: Int, c: C): Boolean
+
+    fun getNotes(i: Int)
+
+    fun getById(i: Int)
+
+    fun getComments(i: Int)
+
+    fun restoreComment(i: Int): Boolean
+
 }
 
